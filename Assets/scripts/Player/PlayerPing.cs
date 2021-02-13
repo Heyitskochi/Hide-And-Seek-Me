8da62cf8f9a8f068c1e661cb1702a2afe2affdaf
Ahlon Sin <ahls@uci.edu>
2021-02-12 22:02:58 -0800

    added smash effect, and worked on the basics of the thrown scanner

---
+++ Assets/scripts/Player/PlayerPing.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System;
+
+public class PlayerPing : MonoBehaviour
+{
+    #region variables
+    //Networking
+    private PhotonView PV;
+
+
+    //firing
+    public GameObject pingProjectile;
+    [SerializeField]
+    private float fireMult = 0.8f;
+    private float currentFirePower = 0;
+    private float maxFirePower = 3;
+    private float cooldown = 2;
+    private float timeToFire;
+    private bool fired = false;
+    private Transform firePos;
+    #endregion
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        firePos = transform.Find("FirePosition");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!fired)
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                charge();
+            }
+            if (Input.GetKeyUp(KeyCode.LeftShift))
+            {
+                fire();
+            }
+        }
+        else
+        {
+            fired = Time.time < timeToFire;
+        }
+      
+
+    }
+
+    private void charge()
+    {
+        if(currentFirePower >= maxFirePower)
+        {
+            fire();
+        }
+        currentFirePower += Time.deltaTime;
+
+        
+    }
+
+    private void fire()
+    {
+        
+        GameObject scanner = Instantiate(pingProjectile, firePos.position, Quaternion.identity);
+        scanner.GetComponent<Rigidbody>().AddForce(transform.forward * currentFirePower * fireMult);
+        fired = false;
+        currentFirePower = 0;
+        timeToFire = Time.time + cooldown;
+    }
+}
---
+++ Assets/scripts/Player/pingBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pingBehavior : MonoBehaviour
+{
+    private int timeLimit = 200;
+
+    private void FixedUpdate()
+    {
+        if (timeLimit-- == 0)
+            Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with " + other.name);
+    }
+}
