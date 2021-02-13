0747d6e9d1cc379b0012b800c433bdfb0c1e6098
vineet <vineetbharot@gmail.com>
2021-02-12 21:53:32 -0800

    [player] added player idle, walking and running animations.

---
+++ Assets/AnimationStateController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+        Debug.Log(animator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool forwardPressed = System.Convert.ToBoolean(Input.GetAxis("Horizontal")) || System.Convert.ToBoolean(Input.GetAxis("Vertical"));
+        bool runPressed = Input.GetKey(KeyCode.LeftShift);
+        if (!isWalking && forwardPressed) {
+            animator.SetBool(isWalkingHash, true);
+        }
+        
+        if (isWalking && !forwardPressed) {
+            animator.SetBool(isWalkingHash, false);
+        }
+
+        if (!isRunning && (forwardPressed && runPressed)) {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed)) {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+}
