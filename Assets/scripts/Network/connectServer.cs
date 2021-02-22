efa63e0d1c3a288edcd16fca599ca74a4416915b
Ahlon Sin <ahls@uci.edu>
2021-02-21 18:09:49 -0800

    ping + some more tweaks

--- Assets/scripts/Network/connectServer.cs
+++ Assets/scripts/Network/connectServer.cs
@@ -1,18 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class connectServer : MonoBehaviour
+using Photon.Pun;
+using Photon.Realtime;
+public class connectServer : MonoBehaviourPunCallbacks,IInRoomCallbacks
 {
+
+    private string gameVersion;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PhotonNetwork.AutomaticallySyncScene = true;
+        gameVersion = "0.001";
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+  
+
+    public void connect()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        /*
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }*/
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(gameObject.name);
+        Debug.Log("Connected to Master");
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = 2 });
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a room");
+        PhotonNetwork.LoadLevel("Sin Scene");
     }
 }
